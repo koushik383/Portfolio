69ab5cc24e001e1d7cab302ad52340b105b3dcf5
Koushik Matta <mattakoushik9@gmail.com>
2025-04-11 21:30:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
   // Code to handle form submission (e.g., send data to backend or display a message)
   alert('Form submitted!');
-}};
+})
